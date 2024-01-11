1b7efbf98328da59d8de28b3fe1bfefe5e4c9601
bijiya <tbijiya22@tbc.edu.np>
2024-01-11 11:08:19 +0545

    week1

--- week5/week5_programs/count_args.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-def count_arguments():
-    argument_count = len(sys.argv) - 1
-    print(f"Number of arguments provided: {argument_count}")
-if __name__ == "__main__":
-    count_arguments()
--- week5/week5_programs/report_platform.py
+++
@@ -1,6 +0,0 @@
-import sys
-def report_platform():
-    platform = sys.platform
-    print(f"Operating System Platform: {platform}")
-if __name__ == "__main__":
-    report_platform()
